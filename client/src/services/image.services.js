1a5eeb93758fa457feb1e55b1742fb6cd104931c
JorgeHermo <82178335+JorgeHermo@users.noreply.github.com>
2022-11-21 10:49:13 +0100

    auth context + auth services + image services + react-bootstrap dependencies

---
+++ client/src/services/image.services.js
@@ -0,0 +1,46 @@
+import axios from "axios"
+
+class ImageService {
+
+    constructor() {
+
+        this.api = axios.create({
+            baseURL: `${process.env.REACT_APP_API_URL}/coasters`
+        })
+
+        this.api.interceptors.request.use((config) => {
+
+            const storedToken = localStorage.getItem("authToken");
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
+    }
+
+    getImages() {
+        return this.api.get("/getAllImages")
+    }
+
+    getOneImage(image_id) {
+        return this.api.get(`/getOneImage/${image_id}`)
+    }
+
+    createImage(imageData) {
+        return this.api.post("/create", imageData)
+    }
+
+    editImage(image_id, data) {
+        return this.api.put(`/edit/${image_id}`, data)
+    }
+
+    deleteImage(image_id) {
+        return this.api.delete(`/delete/${image_id}`)
+    }
+}
+
+const imageService = new ImageService()
+
+export default imageService
